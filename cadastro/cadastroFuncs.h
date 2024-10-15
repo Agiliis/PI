c76baad0b97e7921be12abb3a7174237df41f5f4
Agil <nicholassaraiva16@gmail.com>
2024-10-15 18:02:01 -0300

    smol format

--- cadastro/cadastroFuncs.h
+++ cadastro/cadastroFuncs.h
@@ -11,12 +11,12 @@
 #define ATT '2'
 #define STOP '3'
 
-const char *titulo1      = "\nCADASTRO DE USUÁRIOS \n\n";
-const char *titulo2      = "\nATUALIZACAO DE CADASTROS \n\n";
-const char *opcoes1      = "Digite: \n[1] para cadastrar um novo usuário \n[3] para finalizar os cadastros\n";
-const char *opcoes2      = "Digite: \n[2] para atualizar algum cadastro \n[3] para encerrar o programa\n";
-const char *opcoes2_2    = "[1] atualizar o nome \n[2] atualizar o telefone \n[3] atualizar o endereço \n[4] atualizar o login \n[5] atualizar a senha \n[6] para deslogar \n";
-const char *aviso        = "Insira uma resposta válida!\n";
+const char  *titulo1      = "\nCADASTRO DE USUÁRIOS \n\n",
+            *titulo2      = "\nATUALIZACAO DE CADASTROS \n\n",
+            *opcoes1      = "Digite: \n[1] para cadastrar um novo usuário \n[3] para finalizar os cadastros\n",
+            *opcoes2      = "Digite: \n[2] para atualizar algum cadastro \n[3] para encerrar o programa\n",
+            *opcoes2_2    = "[1] atualizar o nome \n[2] atualizar o telefone \n[3] atualizar o endereço \n[4] atualizar o login \n[5] atualizar a senha \n[6] para deslogar \n",
+            *aviso        = "Insira uma resposta válida!\n";
 
 typedef struct {
     char    login[BUFF_SZ],
