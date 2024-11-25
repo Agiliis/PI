197ffcaafb533fcc3dac39feca23d8db6aaa9d0f
Agil <nicholassaraiva16@gmail.com>
2024-11-25 16:26:00 -0300

    versão de login base

--- emprestimoDeSalas/src/dbg.h
+++ emprestimoDeSalas/src/dbg.h
@@ -1,3 +1,7 @@
-#define dbgc(x) printf("%s = %c\n", #x, x);
-#define dbgs(x) printf("%s = %s\n", #x, x);
-#define dbgi(x) printf("%s = %d\n", #x, x);
+#define DBG_MODE 0
+
+#if DBG_MODE == 1
+    #define dbgc(x) printf("%s = %c\n", #x, x);
+    #define dbgs(x) printf("%s = %s\n", #x, x);
+    #define dbgi(x) printf("%s = %d\n", #x, x);
+#endif
