be48e0e3cd3d67bf8eb68be1b13b7a03214b4f63
Agil <nicholassaraiva16@gmail.com>
2024-12-09 12:58:28 -0300

    modularizado

--- emprestimoDeSalas/src/reservasFuncs.c
+++ emprestimoDeSalas/src/reservasFuncs.c
@@ -15,41 +15,14 @@ const char *horarios[] = {  "07:10", "08:00", "08:50", "09:40", "10:30", "11:20"
                             "20:30", "21:20", "22:10", "23:00"};
 
 /****************************************
-int main() {
-
-    ler_relacao_das_salas("../res/salas.csv", salas);
-    ler_reservas("../res/reservas.csv", reservas);
-
-    
+   
         ESTRUTURA
         - Mostra primeira das salas disponíveis
         - Pergunta se ele quer
             - Caso sim: chama funcao de reservar
             - Caso nao: chama listar_salas_disponiveis
         - Retorna a tela de confirmacao de sala
-    
 
-    char continuar = 'S';
-    while (continuar == 'S' || continuar == 's') {
-        char data[12];
-        escolher_data(data);
-
-        char horario[6];
-        escolher_horario(horario);
-        
-        reservar_sala(reservas, &num_reservas, "../res/reservas.csv", data, horario);
-
-        puts("Deseja reservar outra sala? (S/N)");
-        scanf(" %c", &continuar);
-        getchar(); // bruxaria pra data nao ler '\n'
-
-        limpar_tela();
-    }
-
-    printf("Obrigado por usar o sistema de reservas!\n");
-
-    return 0;
-}
 ********************************************/
 
 void ler_relacao_das_salas(char *pathDoArq, Sala *salas){
@@ -196,6 +169,68 @@ void criar_reserva(Sala salas[], Reserva reservas[], int *num_reservas, const ch
     fclose(arquivo);
 
     printf("Reserva da sala %d realizada com sucesso para a data %s no horário %s.\n", id_sala, data, horario);
+}
+
+void remover_reserva(Sala salas[], Reserva reservas[], int *num_reservas, const char *nome_arquivo, char *data, char *horario){
+    int id_sala;
+    int ok;
+
+    listar_salas(salas, reservas, (*num_reservas), data, horario, "indisponivel");
+
+    ok = 1;
+    do{
+        char buffer[BUFSIZ];
+
+        if(!ok){
+            limpar_tela();
+            listar_salas(salas, reservas, (*num_reservas), data, horario, "indisponivel");
+            puts("Opcao invalida!");
+        }
+
+        puts("Digite o ID da sala que deseja remover");
+
+        fgets(buffer, sizeof(buffer), stdin);
+
+        char *aux = strtok(buffer, "\n");
+        id_sala = strtol(buffer, NULL, 10);
+
+        ok = id_sala != 0 && check_disponibilidade(reservas, (*num_reservas), id_sala, data, horario) == 0;
+    }while(!ok);
+
+    limpar_tela();
+
+    Reserva aux[MAX_RESERVAS];
+    for(int i = 0; i < (*num_reservas); i++){
+        aux[i] = reservas[i];
+    }
+    
+    memset(reservas, 0, sizeof(Reserva));
+
+    for(int i = 0, j = 0; i < (*num_reservas); i++, j++){
+        if(aux[i].id_sala == id_sala){
+            j--;
+            continue;
+        }
+
+        reservas[j] = aux[i];
+    }
+
+    (*num_reservas)--;
+
+    FILE *arquivo = fopen(nome_arquivo, "w");
+    if (!arquivo) {
+        perror("Erro ao abrir o arquivo de reservas");
+        return;
+    }
+
+    fprintf(arquivo, "ID_Sala Data Horario\n");
+    for(int i = 1; i < (*num_reservas); i++){
+        fprintf(arquivo, "%d %s %s\n", reservas[i].id_sala, reservas[i].data, reservas[i].horario);
+    }
+    
+    fclose(arquivo);
+
+    printf("Reserva da sala %d removida com sucesso para a data %s no horário %s.\n", id_sala, data, horario);
 }
 
 /**************************************/
